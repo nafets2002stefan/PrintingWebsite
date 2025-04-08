eaf3b1eeebc4a565ec024a3a04fa1ad196ee237f
Stefan Berestean <stefanberestea@gmail.com>
2025-04-08 16:53:11 +0300

    Filling 'Contacts' page'

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Contacts from '../components/Contacts'
 
 function ContactsPage() {
   return (
-    <></>
+    <main className='relative min-h-screen overflow-x-hidden'>
+        <Contacts/>
+    </main>
   )
 }
 
